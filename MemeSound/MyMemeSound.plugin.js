e908b5f33ca35654659f6ace594d436d572f0a5a
BusyBox13 <95346069+BusyBox13@users.noreply.github.com>
2022-03-02 22:17:48 +0100

    Create MyMemeSound.plugin.js

---
+++ MemeSound/MyMemeSound.plugin.js
@@ -0,0 +1,11 @@
+/**
+ * @name MyMemeSound
+ * @version 0.0.0.1
+ * @description Plays Memetastic sounds depending on what is being sent in chat.
+ * @invite YMqKjWEVxG
+ * @author WarMachine#9792
+ * @authorId 557388558017495046
+ * @authorLink https://github.com/BusyBox13/
+ * @source https://github.com/BusyBox13/
+ * @updateUrl https://raw.githubusercontent.com/Lonk12/BetterDiscordPlugins/main/MemeSounds/MemeSounds.plugin.js
+ */
